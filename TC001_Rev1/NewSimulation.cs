d49c1c25894ecc409e922314f0f69145901a2f74
Pascal Gyger <pascal.gyger@progile.ch>
2024-08-22 11:41:23 +0200

    Improve reporting of example testcase; remove local monitor.

--- TC001_Rev1/NewSimulation.cs
+++ TC001_Rev1/NewSimulation.cs
@@ -4,28 +4,35 @@
 public class NewSimulation : TestCase
 {
 
-    [TestStep(1)]
+    [TestStep(1,
+        TestInput = @"Login with default user credentials")]
     public void Step1(ITester t)
     {
         App.LoginScreen.EmailAddress.Enter($@"welcome@testresults.io");
         App.LoginScreen.Password.Enter($@"HelloWorld");
+        t.Testee.TakeScreenshot($@"Login credentials filled");
         App.LoginScreen.LOGIN.Click(App.LoginScreen.WaitForDisappear);
     }
 
 
-    [TestStep(2)]
+    [TestStep(2,
+        TestInput = @"Read confirmation code from mobile and enter on login confirmation page")]
     public void Step2(ITester t)
     {
         App.LoginConfirmation.MobileLink.Click(App.Mobile.WaitForAppear);
         t.SetVariable("confirmationCode", App.Mobile.ConfirmationCode.ReadValue());
+        t.Testee.TakeScreenshot($@"Mobile Confirmation Code");
         App.Mobile.WebapplicationLink.Click(App.LoginConfirmation.WaitForAppear);
         App.LoginConfirmation.ConfirmationCode.Enter("{{confirmationCode}}");
         App.LoginConfirmation.Language.SelectValue($@"English");
+        t.Testee.TakeScreenshot($@"Confirmation Page");
         App.LoginConfirmation.CONFIRM.Click(App.LoginConfirmation.WaitForDisappear);
+        t.Report.PassStep($@"Login was successfull");
     }
 
 
-    [TestStep(3)]
+    [TestStep(3,
+        TestInput = @"Open last invoice for recent user 'Demo User'")]
     public void Step3(ITester t)
     {
         App.RecentSign_InActivity.RecentSign_InActivityTable.GetRow(App.RecentSign_InActivity.RecentSign_InActivityTable.UserName, $@"Demo User")
@@ -35,7 +42,9 @@ public class NewSimulation : TestCase
     }
 
 
-    [TestStep(4)]
+    [TestStep(4,
+        TestInput = @"Verify the invoice total",
+        ExpectedResults = @"The invoice total is shown as $93.50")]
     public void Step4(ITester t)
     {
         t.Report.PassFailStep(
@@ -44,14 +53,4 @@ public class NewSimulation : TestCase
             $@"The value for {/*element*/ App.InvoicePDF.Total} was {actualValue} instead of the expected value {/*value*/ $@"$93.50"}.");
 
     }
-
-
-
-
-
-
-
-
-
-
 }
